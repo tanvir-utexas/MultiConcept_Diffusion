bff67fd56829216bd51a66aa74f561b24902100e
tanvir-utexas <tanvirmahmud@utexas.edu>
2023-04-16 19:21:15 -0700

    CLIP_with_SD Added

--- scripts/diffusers_clip_sd.sh
+++ scripts/diffusers_clip_sd.sh
@@ -1,15 +1,16 @@
 MODEL_NAME="CompVis/stable-diffusion-v1-4"
 
-accelerate launch src/diffusers_training_clip_sd.py \
+#CUDA_VISIBLE_DEVICES=0 accelerate launch 
+python src/diffusers_training_clip_sd.py \
           --pretrained_model_name_or_path=$MODEL_NAME  \
           --instance_data_dir=./data/cat  \
           --class_data_dir=./real_reg/samples_cat/ \
-          --output_dir=./logs/clip_sd/cat  \
+          --output_dir=./logs/clip_sd/cat_1GPU  \
           --with_prior_preservation --real_prior --prior_loss_weight=1.0 \
           --instance_prompt="photo of a <new1> cat"  \
           --class_prompt="cat" \
-          --resolution=224  \
-          --train_batch_size=4  \
+          --resolution=512  \
+          --train_batch_size=2  \
           --learning_rate=1e-5  \
           --lr_warmup_steps=0 \
           --max_train_steps=250 \
@@ -17,4 +18,4 @@ accelerate launch src/diffusers_training_clip_sd.py \
           --scale_lr --hflip  \
           --modifier_token "<new1>"
 
-# python src/diffusers_sample.py --delta_ckpt logs/cat/delta.bin --ckpt "CompVis/stable-diffusion-v1-4" --prompt "<new1> cat playing with a ball"
+# python src/diffusers_sample.py --delta_ckpt logs/clip_sd/cat_1GPU/delta.bin --ckpt "CompVis/stable-diffusion-v1-4" --prompt "<new1> cat in times square"
--- scripts/diffusers_clip_sd_freeze.sh
+++ scripts/diffusers_clip_sd_freeze.sh
@@ -1,10 +1,10 @@
 MODEL_NAME="CompVis/stable-diffusion-v1-4"
 
-CUDA_VISIBLE_DEVICES=0,1 accelerate launch src/diffusers_training.py \
+accelerate launch src/diffusers_training_clip_sd_freeze.py \
           --pretrained_model_name_or_path=$MODEL_NAME  \
           --instance_data_dir=./data/cat  \
           --class_data_dir=./real_reg/samples_cat/ \
-          --output_dir=./logs/cat_2GPU  \
+          --output_dir=./logs/clip_sd_freeze/cat_4GPU  \
           --with_prior_preservation --real_prior --prior_loss_weight=1.0 \
           --instance_prompt="photo of a <new1> cat"  \
           --class_prompt="cat" \
@@ -17,4 +17,4 @@ CUDA_VISIBLE_DEVICES=0,1 accelerate launch src/diffusers_training.py \
           --scale_lr --hflip  \
           --modifier_token "<new1>"
 
-# python src/diffusers_sample.py --delta_ckpt logs/clip_sd/cat_2GPU/delta.bin --ckpt "CompVis/stable-diffusion-v1-4" --prompt "<new1> cat in times square"
+python src/diffusers_sample.py --delta_ckpt logs/clip_sd/cat_1GPU/delta.bin --ckpt "CompVis/stable-diffusion-v1-4" --prompt "<new1> cat with a dog in times square"
